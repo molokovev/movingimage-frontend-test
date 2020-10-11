20ef9b4f99884e9b4ee8db03d4964b3957bdc512
Eugeny Molokov <eugeny.molokov@fastdev.se>
2020-10-11 17:51:05 +0500

    Implement searching

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { IVideoExtended } from "../../models/video.interface";
+import { FormControl } from "@angular/forms";
+import { ReleaseDateDisplayFormat, stringifyVideoForSearch } from "../../models/video.helpers";
 
 @Component({
   selector: "app-list",
@@ -10,13 +12,20 @@ import { IVideoExtended } from "../../models/video.interface";
 export class ListComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
   videos?: IVideoExtended[];
-  searchString = "";
+  searchInput = new FormControl("");
+  videosFiltered?: IVideoExtended[];
+  releaseDateDisplayFormat = ReleaseDateDisplayFormat;
 
   ngOnInit(): void {
     this.route.data.subscribe((data: { videos: IVideoExtended[] }) => {
       this.videos = data.videos;
+      this.videosFiltered = this.videos.slice();
     });
   }
 
-  search() {}
+  search() {
+    this.videosFiltered = this.videos.filter((vid) =>
+      stringifyVideoForSearch(vid).includes(this.searchInput.value)
+    );
+  }
 }
--- src/app/models/video.helpers.ts
+++ src/app/models/video.helpers.ts
@@ -1,6 +1,8 @@
 import { IAuthor } from "./author.interface";
 import { ICategory } from "./category.interface";
 import { IVideo, IVideoExtended } from "./video.interface";
+import { HighestQualityFormatPipe } from "../pipes/highest-quality-format.pipe";
+import { DatePipe } from "@angular/common";
 
 export function extendVideo(
   vid: IVideo,
@@ -15,3 +17,15 @@ export function extendVideo(
       .map((x) => x.name),
   };
 }
+
+export function stringifyVideoForSearch(vid: IVideoExtended): string {
+  return JSON.stringify([
+    vid.name,
+    vid.authorName,
+    vid.categoryNames.join(),
+    new HighestQualityFormatPipe().transform(vid.formats),
+    new DatePipe("en-EN").transform(vid.releaseDate, ReleaseDateDisplayFormat),
+  ]);
+}
+
+export const ReleaseDateDisplayFormat = "dd.M.yyyy";
